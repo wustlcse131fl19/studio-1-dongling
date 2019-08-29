aaf286ec58090499260cb8afb648dec1a75440ba
Cendi Ling <cendiling@wustl.edu>
2019-08-29 15:16:29 -0500

    Studio 1 finished

--- src/studio1/Average.java
+++ src/studio1/Average.java
@@ -10,6 +10,5 @@ public class Average {
         double average = (double) (t1 + t2)/2;
         System.out.println("Average of " + t1 + " degrees and " + t2 + " degrees is " + average + ".");
 
-
     }
 }
